77cb7401f7507b19f27cf3281e45458519960854
Callen <Callenowy@users.noreply.github.com>
2021-04-24 00:19:58 +0200

    chore: refactor webpack configuration, update packages (#1)

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,52 +1,72 @@
+const glob = require('glob');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const paths = require('./paths');
+
+const generateHTMLPlugins = () =>
+  glob.sync('./src/**/*.html').map(
+    dir =>
+      new HtmlWebpackPlugin({
+        favicon: paths.src + '/static/favicon.png',
+        template: dir, //paths.src + '/template.html', // template file
+        filename: path.basename(dir), //'index.html', // output file
+      })
+  );
+
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, '../src/scripts/index.js'),
-  },
+  entry: [`${paths.src}/index.js`],
+
   output: {
-    path: path.join(__dirname, '../build'),
-    filename: 'js/[name].js',
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      name: false,
-    },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{ from: path.resolve(__dirname, '../public'), to: 'public' }],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../src/index.html'),
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, '../src'),
-    },
-  },
+
   module: {
     rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      { test: /\.js$/, use: ['babel-loader'] },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+
       {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          },
-        },
+        test: /\.html$/i,
+        loader: 'html-loader',
       },
     ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    ...generateHTMLPlugins(),
+  ],
+
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': paths.src,
+    },
   },
 };
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,36 +1,55 @@
-const Webpack = require('webpack');
-const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const paths = require('./paths');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
-  bail: true,
+  devtool: false,
   output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+    path: paths.build,
+    publicPath: '/',
+    filename: 'js/[name].[contenthash].bundle.js',
   },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css',
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+              modules: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), '...'],
+    runtimeChunk: {
+      name: 'runtime',
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  stats: 'errors-only',
 });
