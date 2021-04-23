7ae85198421cc16d0b443b1a04787a1e05a00529
Callen <naporaradekbhd@gmail.com>
2021-04-24 00:14:49 +0200

    chore: refactor webpack configuration, update packages

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -4,49 +4,44 @@ const { merge } = require('webpack-merge');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
 const common = require('./webpack.common.js');
+const paths = require('./paths');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'eval-cheap-source-map',
-  output: {
-    chunkFilename: 'js/[name].chunk.js',
-  },
+
+  devtool: 'inline-source-map',
+
   devServer: {
-    inline: true,
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: false,
+    compress: true,
     hot: true,
+    port: 3000,
   },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    new StylelintPlugin({
-      files: path.join('src', '**/*.css'),
-    }),
-  ],
+
   module: {
     rules: [
-      //   {
-      //     test: /\.js$/,
-      //     include: path.resolve(__dirname, '../src'),
-      //     enforce: 'pre',
-      //     loader: 'eslint-loader',
-      //     options: {
-      //       emitWarning: true,
-      //     },
-      //   },
       {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, '../src'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'postcss-loader'],
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1, modules: true },
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
       },
     ],
   },
+
+  plugins: [new Webpack.HotModuleReplacementPlugin(), new StylelintPlugin()],
+
+  stats: {
+    colors: true,
+  },
+
+  devtool: 'source-map',
 });
